4d9b3d001796f3bfe30c0a47d3febc56597b378d
minsoo kim <zlemzlem5656@naver.com>
2021-11-10 00:52:55 +0900

    project init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
+<!-- @format -->
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div>hi</div>
 </template>
+
+<script>
+  export default {}
+</script>
+
+<style></style>
